d65f21dbb61beeb9c117c518ae5badd87334778b
Dulitha Nanayakkara <dulitha.nanayakkara@otelier.io>
2025-01-10 21:42:08 +0530

    initial-commit

---
+++ src/DevOpsChallenge.SalesApi/Program.cs
@@ -0,0 +1,21 @@
+using DevOpsChallenge.SalesApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+// Set console title
+Console.Title = "DevOps Challenge Sales API";
+
+// Build and run host
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddEnvironmentVariables("APP_");
+    })
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStartup<Startup>();
+        webBuilder.ConfigureKestrel(options => options.AddServerHeader = false); // Remove "Server: Kestrel" header
+    })
+    .RunConsoleAsync();
